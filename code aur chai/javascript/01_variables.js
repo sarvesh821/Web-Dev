2c369fa054d6167cac27071059d225864953be72
sarvesh821 <96337417+sarvesh821@users.noreply.github.com>
2022-12-28 16:57:46 +0000

    commit 3

---
+++ code aur chai/javascript/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=123;
+let accountEmail="abc@gmail.com";
+var accountPassword="7979";
+accountCity="jaipur";
+console.log(accountId);
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+//dont use var variable as it dosent define the scope
---
+++ code aur chai/javascript/02_datatypes.js
@@ -0,0 +1,10 @@
+//alert(3+3); we are using node js not browser
+let accountName="sarvesh goyal"
+let accountAge=23;
+let isValid=true;
+let accountCity;
+let accontRoll=null
+console.log(typeof accontRoll);
+console.log(typeof accountCity);
+console.log(typeof isValid);
+console.log(typeof accountAge);
