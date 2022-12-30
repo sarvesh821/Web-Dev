ba3296ea3d7db92ceac34a82fa7c06cf65aafa61
sarvesh821 <sarveshgoyal4152@gmail.com>
2022-12-30 22:14:42 +0530

    conversion

---
+++ code aur chai/javascript/03_datatypesconversions.js
@@ -0,0 +1,16 @@
+let num = "33";
+console.log(typeof num);
+let valueofnum = Number(num);
+console.log(valueofnum);
+//true=1;
+//false=0;
+let isbollean = 1;
+let booleanisbollean = Boolean(isbollean);
+console.log(booleanisbollean);
+//1=>true;0=>false;
+//""=false;
+//"hitesh"=true
+let somenumber = 33;
+let stringnumber = String(somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber);
